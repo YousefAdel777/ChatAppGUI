5e1f63309387a293b6a115151b985c97c7351f07
Yusuf Mahmud <yusufmahmud2005@gmail.com>
2025-05-12 21:35:55 +0300

    TestSolution

---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Privacy_FILE D:/QT Designs/Privacy/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Privacy.exe)
+set(__QT_DEPLOY_TARGET_Privacy_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Privacy_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
