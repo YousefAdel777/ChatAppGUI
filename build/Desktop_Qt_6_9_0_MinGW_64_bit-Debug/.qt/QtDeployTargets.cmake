732990320fbc38f37cd38878c93e3349f4413981
Yusuf Mahmud <yusufmahmud2005@gmail.com>
2025-05-18 00:08:40 +0300

    Untrack Build Files

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ChatApp_FILE E:/Projects/qtesting/ChatApp/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/ChatApp.exe)
-set(__QT_DEPLOY_TARGET_ChatApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ChatApp_RUNTIME_DLLS E:/Programes/QT/6.9.0/mingw_64/bin/Qt6Widgets.dll;E:/Programes/QT/6.9.0/mingw_64/bin/Qt6Multimedia.dll;E:/Programes/QT/6.9.0/mingw_64/bin/Qt6Gui.dll;E:/Programes/QT/6.9.0/mingw_64/bin/Qt6Network.dll;E:/Programes/QT/6.9.0/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/deploy_ChatApp_b296194f9a.cmake
+++
@@ -1,8 +0,0 @@
-include("E:/Projects/qtesting/ChatApp/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ChatApp-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase;qtmultimedia")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE E:/Projects/qtesting/ChatApp/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/ChatApp.exe
-    GENERATE_QT_CONF
-)
