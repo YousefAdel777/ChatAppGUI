97cbf2d62c4fc5ba164b1f2069da4ea71c052a92
YousefAdel777 <www.yoyoadel@gmail.com>
2025-06-16 22:25:36 +0300

    remove: build

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-RelWithDebInfo/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtLearning_FILE C:/Users/Dell/Desktop/chat-2/QtLearning/build/Desktop_Qt_6_9_0_MinGW_64_bit-RelWithDebInfo/QtLearning.exe)
-set(__QT_DEPLOY_TARGET_QtLearning_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_QtLearning_RUNTIME_DLLS D:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Multimedia.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Quick.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6QmlMeta.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6QmlWorkerScript.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6QmlModels.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Qml.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Network.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6OpenGL.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
